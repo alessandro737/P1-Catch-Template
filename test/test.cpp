8e83ab0e9688580630f3823e8537973e7a201cbe
alessandro737 <ALEG0008@HOTMAIL.COM>
2024-09-16 18:44:40 -0400

    commented out test cases for future use

--- test/test.cpp
+++ test/test.cpp
@@ -7,16 +7,16 @@
 using namespace std;
 
 // the syntax for defining a test is below. It is important for the name to be unique, but you can group multiple tests with [tags]. A test can have [multiple][tags] using that syntax.
-TEST_CASE("Example Test Name - Change me!", "[flag]"){
-	// instantiate any class members that you need to test here
-	int one = 1;
-
-	// anything that evaluates to false in a REQUIRE block will result in a failing test 
-	REQUIRE(one == 0); // fix me!
-
-	// all REQUIRE blocks must evaluate to true for the whole test to pass
-	REQUIRE(false); // also fix me!
-}
+//TEST_CASE("Example Test Name - Change me!", "[flag]"){
+//	// instantiate any class members that you need to test here
+//	int one = 1;
+//
+//	// anything that evaluates to false in a REQUIRE block will result in a failing test
+//	REQUIRE(one == 0); // fix me!
+//
+//	// all REQUIRE blocks must evaluate to true for the whole test to pass
+//	REQUIRE(false); // also fix me!
+//}
 
 TEST_CASE("Test 2", "[flag]"){
 	// you can also use "sections" to share setup code between tests, for example:
